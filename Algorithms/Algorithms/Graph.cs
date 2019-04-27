233494b7e0a9995f2ada482306b813377a8d37a2
Avinash <1808avinash@gmail.com>
2019-04-27 00:08:25 -0400

    graph datastructure creation and dfs algorithm

---
+++ Algorithms/Algorithms/Graph.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+    public class Graph
+    {
+        int vertices;
+        Dictionary<int, List<int>> adj;
+
+        public int GetVertices() => this.vertices;
+
+        public Graph(int v)
+        {
+            this.vertices = v;
+            this.adj = new Dictionary<int, List<int>>();
+
+            for (int i = 0; i < v;++i)
+            {
+                this.adj.Add(i, new List<int>());
+            }
+        }
+        public void AddEdge(int orig, int dest)
+        {
+            this.adj[orig].Add(dest);
+            this.adj[dest].Add(orig);
+        }
+        public IList<int> GetAdjacentNodes(int o)
+        {
+            return this.adj[o];
+        }
+    }
+}
