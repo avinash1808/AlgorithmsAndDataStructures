118b9784e9c341e47bbb0223e0edc18a36b97bcb
avinash <1808avinash@gmail.com>
2019-05-22 11:30:41 -0400

    Started Kruskal MST

---
+++ Algorithms/AlgoAndDS/Algorithms/Graphs/KruskalMST.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoAndDS.DataStructures;
+
+namespace AlgoAndDS.Algorithms.Graphs
+{
+    class KruskalMST
+    {
+        private WeightedUnDirectedGraph G;
+        private Queue<WeightedUndirectedEdge> MSTEdges;
+        private double TotalWeight;
+        
+
+        public KruskalMST(WeightedUnDirectedGraph g)
+        {
+            this.G = g;
+            this.MSTEdges = new Queue<WeightedUndirectedEdge>();
+            this.TotalWeight = 0;
+
+
+
+
+        }
+
+        public IEnumerable<WeightedUndirectedEdge> Edges()
+        {
+            return this.MSTEdges;
+        }
+
+    }
+}
